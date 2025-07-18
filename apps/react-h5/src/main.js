397b6b01241c2dad9d86d0784df88f1f4a120d22
唐贝 <itangbei@sina.com>
2025-07-18 17:03:58 +0800

    代码优化

--- apps/react-h5/src/main.js
+++ apps/react-h5/src/main.js
@@ -1,11 +1,7 @@
+import { jsx as _jsx } from "react/jsx-runtime";
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import '@tang/styles/index.css';
 import App from './App';
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')).render(_jsx(StrictMode, { children: _jsx(App, {}) }));
